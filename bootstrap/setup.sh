752b569c9fa88466b72d738d900ddc5cbe85dade
Scott Yoder <syoder89@gmail.com>
2025-10-22 17:04:37 -0500

    fix ArgoCD bootstrap

---
+++ bootstrap/setup.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+ARGOCD_DIR=../applications/argocd
+helm dependency update ${ARGOCD_DIR}
+helm template -n argocd argocd ${ARGOCD_DIR} --values ${ARGOCD_DIR}/values/prod.yaml | kubectl apply -f -
+kubectl apply -f manifests/
